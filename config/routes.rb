ae1874b91f9875fa61e21d7923f9f0eca5e10a97
Chris Millar <chris.millar36@gmail.com>
2024-08-05 22:10:06 -0400

    enhance: redirect all non-specified paths to /app

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root :to => redirect("/app")
+  match "*path", to: redirect("/app"), via: :all
 end
