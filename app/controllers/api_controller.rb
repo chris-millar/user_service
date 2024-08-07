8fc2639b58ed8480b0f8783ff41607dc736b48a4
Chris Millar <chris.millar36@gmail.com>
2024-08-07 15:56:52 -0400

    create a wrapper concern ApiPagination to abstract pagy dependency + colocate all pagination related API helpers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,29 +1,21 @@
 class ApiController < ActionController::Base
-  include Pagy::Backend
+  include ApiPagination
   include ApiFiltering
   include ApiSorting
 
   def load_requested_records
-    @pagy, @records = paginate(apply_sort(apply_filters(base_record_scope)))
+    @records = paginate(apply_sort(apply_filters(base_record_scope)))
   end
 
   def formatted_response_json
     {
       data: serialized_records,
       metadata: {
-        paging: pagy_metadata(@pagy),
+        paging: paging_metadata,
         filters: applied_filters,
         sort: applied_sorts
       }
     }
-  end
-
-  def page_limit
-    params.permit(:page_limit)[:page_limit]
-  end
-
-  def paginate(scope)
-    pagy(scope, limit: page_limit || Api::PaginationHelper.limit)
   end
 
   def base_record_scope
