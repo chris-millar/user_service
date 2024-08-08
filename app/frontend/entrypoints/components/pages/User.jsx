03333c6f4a27fa31a27196eb482ca51079eb5f00
Chris Millar <chris.millar36@gmail.com>
2024-08-08 11:49:40 -0400

    get a basic view of a User setup - include Material UI for a component library - excluding dateCreated rendering as a date - just use hardcoded user data for now

--- app/frontend/entrypoints/components/pages/User.jsx
+++ app/frontend/entrypoints/components/pages/User.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+
+const Field = ({label, value}) => {
+  return (
+    <TextField disabled variant="outlined" defaultValue={value} label={label}/>
+  )
+}
 
 export const User = () => {
+  const user = {
+    firstName: "Bob",
+    lastName: "Smith",
+    email: "bob.smith@email.com",
+    profession: "Teacher",
+    country: "United States",
+    city: "Atlanta",
+    dateCreated: "2020-01-01T00:00:00Z"
+  }
+
   return (
-    <div>User</div>
+    <>
+      <div>User</div>
+      <br/>
+      <Stack spacing={2}>
+        <Field label={"First Name"} value={user.firstName}/>
+        <Field label={"Last Name"} value={user.lastName}/>
+        <Field label={"Email"} value={user.email}/>
+        <Field label={"Profession"} value={user.profession}/>
+        <Field label={"City"} value={user.city}/>
+        <Field label={"Country"} value={user.country}/>
+        <div>{user.dateCreated}</div>
+      </Stack>
+    </>
   );
 }
