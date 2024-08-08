cd804c9ce7ef74fba790f19dc2a1cb7f25616032
Chris Millar <chris.millar36@gmail.com>
2024-08-08 10:52:33 -0400

    add react routing + add some placeholder components and routes

--- app/frontend/entrypoints/components/App.jsx
+++ app/frontend/entrypoints/components/App.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './Home';
+import { UserList } from './UserList';
+import { User } from './User';
+import { NotFound } from './NotFound';
+import { Link } from 'react-router-dom';
 
 export const App = () => {
-  return <h1>Hello, App from React!</h1>;
-};
+  return (
+    <Router basename="/app">
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/users">UserList</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/users" element={<UserList/>}/>
+        <Route path="/users/:id" element={<User/>}/>
+        <Route path="*" element={<NotFound/>}/> {/* 404 page */}
+      </Routes>
+    </Router>
+  );
+}
