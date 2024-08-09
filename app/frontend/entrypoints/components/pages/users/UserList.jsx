fa9fa92b11a32b3c87c91732b719589f8c4b34ac
Chris Millar <chris.millar36@gmail.com>
2024-08-08 21:22:35 -0400

    extract DataGrid config from the UserList component

--- app/frontend/entrypoints/components/pages/users/UserList.jsx
+++ app/frontend/entrypoints/components/pages/users/UserList.jsx
@@ -1,33 +1,9 @@
-import React, {useState, useMemo, useRef, useCallback} from 'react';
+import React, {useState, useMemo, useRef } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { DateTime } from 'luxon';
-import { Link } from 'react-router-dom';
-import { useUsersQuery } from '../../services/userServices';
+import { useUsersQuery } from '../../../services/userServices';
+import { columns } from './gridConfig';
 
 const PAGE_SIZE = 50;
-
-const renderIdAsLink = ({ value }) => {
-  const path = `/users/${value}`
-  return (<Link id={path} to={path}>{value}</Link>)
-}
-
-const columns = [
-  { field: 'id', headerName: 'ID', type: 'number', width: 75, renderCell: renderIdAsLink },
-  { field: 'first_name', headerName: 'First name', type: 'string', width: 100 },
-  { field: 'last_name', headerName: 'Last name', type: 'string', width: 100 },
-  { field: 'email', headerName: 'Email', type: 'string', width: 300 },
-  { field: 'profession', headerName: 'Profession', type: 'string', width: 200 },
-  { field: 'city', headerName: 'City', type: 'string', width: 200 },
-  { field: 'country', headerName: 'Country', type: 'string', width: 200 },
-  {
-    field: 'date_created',
-    headerName: 'Date Created',
-    type: 'date',
-    width: 180,
-    valueFormatter: (isoString) => DateTime.fromISO(isoString).toLocaleString(DateTime.DATE_SHORT),
-    sortComparator: (dateTime1, dateTime2) => dateTime1.toMillis() - dateTime2.toMillis(),
-  },
-];
 
 export const UserList = () => {
   const [paginationModel, setPaginationModel] = useState({
@@ -68,6 +44,5 @@ export const UserList = () => {
         />
       </div>
     </>
-)
-  ;
+  );
 }
