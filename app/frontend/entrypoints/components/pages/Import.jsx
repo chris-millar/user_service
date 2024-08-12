547b11490a4cb98085a6f6c6339afee39e1134a3
Chris Millar <chris.millar36@gmail.com>
2024-08-12 18:44:22 -0400

    make a link from a given import to view the users that were in that import - make Import page record count a link to the users page with that import_id filter applied

--- app/frontend/entrypoints/components/pages/Import.jsx
+++ app/frontend/entrypoints/components/pages/Import.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { DateTime } from 'luxon';
-import { useParams } from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { DateField } from '@mui/x-date-pickers/DateField';
@@ -14,6 +14,36 @@ const Field = ({label, value}) => {
     <TextField disabled variant="outlined" defaultValue={value} label={label}/>
   )
 }
+
+const RecordCountLinkToUsers = ({ label, importId, recordCount }) => {
+  return (
+    <Box position="relative" display="inline-block">
+      <TextField
+        defaultValue={recordCount}
+        disabled
+        fullWidth
+        label={label}
+      />
+      <Link
+        to={`/users?import_id=${importId}`}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textDecoration: 'none',
+          color: 'inherit',
+          pointerEvents: importId ? 'auto' : 'none',
+        }}
+      >
+      </Link>
+    </Box>
+  );
+};
 
 export const Import = () => {
   const { id } = useParams();
@@ -32,7 +62,7 @@ export const Import = () => {
       <Stack spacing={2}>
         <Field label={"Filename"} value={import_record.filename}/>
         <Field label={"Status"} value={import_record.status}/>
-        <Field label={"Record Count"} value={import_record.record_count}/>
+        <RecordCountLinkToUsers label={"Record Count"} recordCount={import_record.record_count} importId={id}/>
         <DateField disabled label={"Performed At"} defaultValue={DateTime.fromISO(import_record.performed_at)} />
       </Stack>
     </>
