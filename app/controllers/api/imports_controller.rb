b5bb62044c99a9469be9afbc040bfedb18cf67f9
Chris Millar <chris.millar36@gmail.com>
2024-08-12 14:15:54 -0400

    refactor to use guards

--- app/controllers/api/imports_controller.rb
+++ app/controllers/api/imports_controller.rb
@@ -1,18 +1,15 @@
 class Api::ImportsController < ApiController
   def create
     file = params.require(:file)
-    if file.present?
-      render json: { error: 'Invalid file format' }, status: :unprocessable_entity unless file.content_type == 'text/csv'
+    return render json: { error: 'No file uploaded' }, status: :unprocessable_entity unless file.present?
+    return render json: { error: 'Invalid file format' }, status: :unprocessable_entity unless file.content_type == 'text/csv'
 
-      import = Importer.import(file)
+    import = Importer.import(file)
 
-      if import.status === "done"
-        render json: import, status: :ok
-      else
-        render json: import, status: :bad_request
-      end
+    if import.status === "done"
+      render json: import, status: :ok
     else
-      render json: { error: 'No file uploaded' }, status: :unprocessable_entity
+      render json: import, status: :bad_request
     end
   end
 end
