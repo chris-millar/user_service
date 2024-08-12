011bfab5528e29ae24195dd3b4204c58359ad37d
Chris Millar <chris.millar36@gmail.com>
2024-08-12 15:28:25 -0400

    push all new importing into a modal + fix bug: importing again after failed wasn't working - bug: needed to clear the set file state on the hidden file-input element

--- app/frontend/entrypoints/components/pages/imports/NewImport.jsx
+++ app/frontend/entrypoints/components/pages/imports/NewImport.jsx
@@ -7,8 +7,20 @@ import Alert from '@mui/material/Alert';
 import CircularProgress from '@mui/material/CircularProgress';
 import { Link } from 'react-router-dom';
 import { useImportMutation } from '../../../services/importServices';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
-const PAGE_SIZE = 50;
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const VisuallyHiddenInput = styled('input')({
   clip: 'rect(0 0 0 0)',
@@ -25,10 +37,13 @@ const VisuallyHiddenInput = styled('input')({
 export const NewImport = ({ refetchImports }) => {
   const [selectedFile, setSelectedFile] = useState(null);
 
+  const [open, setOpen] = React.useState(false);
+
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
       setSelectedFile(file);
+      setOpen(true);
     }
   };
 
@@ -37,6 +52,12 @@ export const NewImport = ({ refetchImports }) => {
       refetchImports()
     }
   });
+  const closeModal = () => {
+    setOpen(false);
+    setSelectedFile(null);
+    document.getElementById('file-input').value = '';
+    mutation.reset()
+  }
 
   return (
     <>
@@ -49,29 +70,38 @@ export const NewImport = ({ refetchImports }) => {
           startIcon={<CloudUploadIcon/>}
         >
           Upload file
-          <VisuallyHiddenInput type="file" onChange={handleFileChange}/>
+          <VisuallyHiddenInput type="file" onChange={handleFileChange} id="file-input"/>
         </Button>
-        {selectedFile && (
-          <>
-            <Typography variant="body1" style={{marginTop: '10px'}}>
-              Selected File: {selectedFile.name}
-            </Typography>
-            <Button
-              component="label"
-              variant="contained"
-              onClick={() => mutation.mutate({file: selectedFile})}
-            >Import File!</Button>
-            {mutation.isLoading && <CircularProgress>Importing...</CircularProgress>}
-            {mutation.isSuccess && (
+        <Modal
+          open={open}
+          onClose={closeModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            {selectedFile && (
               <>
-                <Alert severity="success">Import was successful!</Alert>
-                <Link to={`/users?import_id=${mutation.data.id}`}>View Users from import</Link>
+                <Typography variant="body1" style={{marginTop: '10px'}}>
+                  Selected File: {selectedFile.name}
+                </Typography>
+                <Button
+                  component="label"
+                  variant="contained"
+                  onClick={() => mutation.mutate({file: selectedFile})}
+                >Import File!</Button>
+                {mutation.isLoading && <CircularProgress>Importing...</CircularProgress>}
+                {mutation.isSuccess && (
+                  <>
+                    <Alert severity="success">Import was successful!</Alert>
+                    <Link to={`/users?import_id=${mutation.data.id}`}>View Users from import</Link>
+                  </>
+                )}
+                {mutation.isError &&
+                  <Alert severity="error">Error importing file: {mutation.error.response.data.error}</Alert>}
               </>
             )}
-            {mutation.isError &&
-              <Alert severity="error">Error importing file: {mutation.error.response.data.error}</Alert>}
-          </>
-        )}
+          </Box>
+        </Modal>
       </div>
     </>
   );
