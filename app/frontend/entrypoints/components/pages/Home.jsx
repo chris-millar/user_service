fe52a511b6973b15e54b24f10fd5f62cd6acdeb1
Chris Millar <chris.millar36@gmail.com>
2024-08-12 20:53:20 -0400

    styling: improve the Home page look and feel

--- app/frontend/entrypoints/components/pages/Home.jsx
+++ app/frontend/entrypoints/components/pages/Home.jsx
@@ -1,6 +1,9 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Card, CardActions, CardContent, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import { Link as RouterLink } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 export const Home = () => {
   const [userId, setUserId] = useState('');
@@ -8,54 +11,148 @@ export const Home = () => {
 
   return (
     <>
-      <h1>Landing Page for the User Service app</h1>
-      <br/>
-      The following features are currently available:
-      <ul>
-        <li>
-          <div style={{display: "flex", alignItems: "center"}}>
-            <div>Viewing a single user via <Link to={`/users/${userId}`}>{`/users/${userId}`}</Link> for a specified ID
-            </div>
-            &nbsp;&nbsp;
-            <TextField style={{width: "75px"}} label="id" size="small" onChange={(e) => setUserId(e.target.value)}/>
-          </div>
-        </li>
-        <li>
-          Viewing all users via <Link to="/users">/users</Link> with the ability to:
-          <ul>
-            <li>paginate through all results via the table. Page size is fixed to 25.</li>
-            <li>live server filter by profession</li>
-            <li>
-              live server filter by a date range.
-              The range is inclusive of the min and max dates (aka gte and lte).
-              You may specify one or both sides of the range.
-            </li>
-            <li>
-              the table provides:
-              <ul>
-                <li>client sorting and filtering by column on the loaded page</li>
-                <li>customizing which columns are displayed</li>
-              </ul>
-            </li>
+      <Typography variant="h2" component="h2" gutterBottom>
+        Landing Page for the User Service app
+      </Typography>
+      <Typography variant="body1" paragraph>
+        The following features are currently available:
+      </Typography>
+      <Grid container spacing={2} alignItems="stretch">
+        <Grid item xs={12}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">View a Single User</Typography>
+              <Box display="flex" alignItems="center">
+                <Typography variant="body2">
+                  View a single user by entering their ID.
+                </Typography>
+                <TextField
+                  style={{width: "75px", marginLeft: "15px"}}
+                  label="id"
+                  size="small"
+                  onChange={(e) => setUserId(e.target.value)}
+                />
+              </Box>
+            </CardContent>
+            <CardActions sx={{ paddingLeft: '16px', paddingBottom: '16px' }}>
+              <Typography
+                component={RouterLink}
+                to={`/users/${userId}`}
+                sx={{
+                  textDecoration: 'none',
+                  color: 'primary.main',
+                  '&:hover': {
+                    textDecoration: 'underline',
+                  },
+                }}
+              >
+                Go to user with ID { userId }
+              </Typography>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">View All Users</Typography>
+              <Typography variant="body2">
+                View all users with filtering and pagination options.
+                <ul>
+                  <li>paginate through all results via the table. Page size is fixed to 25.</li>
+                  <li>live server filter by profession</li>
+                  <li>
+                    live server filter by a date range.
+                    The range is inclusive of the min and max dates (aka gte and lte).
+                    You may specify one or both sides of the range.
+                  </li>
+                  <li>
+                    the table provides:
+                    <ul>
+                      <li>client sorting and filtering by column on the loaded page</li>
+                      <li>customizing which columns are displayed</li>
+                    </ul>
+                  </li>
 
-          </ul>
-        </li>
-        <li>
-          <div style={{display: "flex", alignItems: "center"}}>
-            <div>Viewing a single import via&nbsp;&nbsp;<Link to={`/imports/${importId}`}>{`/imports/${importId}`}</Link> for a specified ID
-            </div>
-            &nbsp;&nbsp;
-            <TextField style={{width: "75px"}} label="id" size="small" onChange={(e) => setImportId(e.target.value)}/>
-          </div>
-        </li>
-        <li>
-          Viewing previous Imports and starting a new one from a csv of Users from your machine via
-          &nbsp;
-          <Link to={`/imports`}>
-            /imports
-          </Link>
-        </li>
-      </ul>
+                </ul>
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ paddingLeft: '16px', paddingBottom: '16px' }}>
+              <Typography
+                component={RouterLink}
+                to="/users"
+                sx={{
+                  textDecoration: 'none',
+                  color: 'primary.main',
+                  '&:hover': {
+                    textDecoration: 'underline',
+                  },
+                }}
+              >
+                View users
+              </Typography>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">View a Single Import</Typography>
+              <Box display="flex" alignItems="center">
+                <Typography variant="body2">
+                  View a single import by entering its ID.
+                </Typography>
+                <TextField
+                  style={{width: "75px", marginLeft: "15px"}}
+                  label="id"
+                  size="small"
+                  onChange={(e) => setImportId(e.target.value)}
+                />
+              </Box>
+            </CardContent>
+            <CardActions sx={{ paddingLeft: '16px', paddingBottom: '16px' }}>
+              <Typography
+                component={RouterLink}
+                to={`/imports/${importId}`}
+                sx={{
+                  textDecoration: 'none',
+                  color: 'primary.main',
+                  '&:hover': {
+                    textDecoration: 'underline',
+                  },
+                }}
+              >
+                Go to import with ID { importId }
+              </Typography>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">View All Imports + Import new Users</Typography>
+              <Typography variant="body2">
+                View all previous Imports.
+                You can also upload a new csv and import those users into the system.
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ paddingLeft: '16px', paddingBottom: '16px' }}>
+              <Typography
+                component={RouterLink}
+                to="/imports"
+                sx={{
+                  textDecoration: 'none',
+                  color: 'primary.main',
+                  '&:hover': {
+                    textDecoration: 'underline',
+                  },
+                }}
+              >
+                View imports or create a new import
+              </Typography>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
     </>
   );
 }
